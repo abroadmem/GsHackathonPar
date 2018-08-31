cfb85417ff927abd1ff96d727c95a1f6cf2e0d8f
shiba <project.funnel@gmail.com>
2018-09-01 02:02:43 +0900

    [コンポーネント][哲さん用] カメラ起動、QR読み込み

---
+++ components/onCamera.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+    v-dialog(v-model="dialog" width="80%" )
+        v-card: v-container ここにカメラ画像を表示、読み込み終わったらthis.dialog = falseにしてダイアログを閉じる
+</template>
+
+<script>
+//ここでプラグイン読み込み
+export default {
+    data(){
+        return{
+            //ダイアログの開閉
+            dialog : false
+        }
+    },
+    methods:{
+        open(){
+            //親コンポーネントのButtonで開く
+            this.dialog = true
+            //何らかの処理
+            //VIDEOの起動についてはhttps://qiita.com/nkg/items/be89d8aebda6509ce2e1
+            //QRコードの読み込みについてはhttps://qiita.com/TakenoriHirao/items/8906fb01e5bb542dd0f9
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
