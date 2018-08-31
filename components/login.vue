87c692260542632ea5a36ed62da540dcd3c1dffe
shiba <project.funnel@gmail.com>
2018-09-01 07:12:18 +0900

    [index] ログインForm追加

---
+++ components/login.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+    v-container
+        div(style="width:300px;margin:50px auto;")
+            v-text-field(
+                label="メールアドレス"
+                v-model="email"
+                type="email"
+                )
+            v-text-field(
+                label="パスワード"
+                v-model="password"
+                type="password"
+                )
+            v-btn(
+                depressed
+                color="info"
+                @click="onClick"
+            ) ログイン
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email:null,
+            password:null
+        }
+    },
+    methods:{
+        onClick(){
+            const email = this.email;
+            const password = this.password;
+            this.$firebase.auth().signInWithEmailAndPassword(email, password)
+                .then(()=>location.href="/admin")
+                .catch(function(error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    alert(errorMessage)
+                });
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,13 +11,15 @@
         icon
         to="/signage")
         v-icon(mx-3) view_quilt
-    p ここはログイン
+    Login
 </template>
 
 <script>
-
+import Login from '~/components/login';
 export default {
-
+  components:{
+    Login,
+  },
 }
 </script>
 
